be68cc36b9d1b704b8e61b34dff9fb07403ae6d5
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-11-24 22:43:25 +0000

    initialize Layout and Runner files

---
+++ Layout.java
@@ -0,0 +1,18 @@
+///*
+// * CSCI 185 M02
+// * Fall 2025
+// * Final Programming Project
+// * Author: Benjamin Chau, Anthony Coates, Jeffrey Perez
+// * Date: 11/24/2025
+// */
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class Layout extends JFrame {
+    public Layout(){
+
+    }
+}
---
+++ Runner.java
@@ -0,0 +1,17 @@
+///*
+// * CSCI 185 M02
+// * Fall 2025
+// * Final Programming Project
+// * Author: Benjamin Chau, Anthony Coates, Jeffrey Perez
+// * Date: 11/24/2025
+// */
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Runner[
+    public static void main(String[] args){
+        
+    }
+]
