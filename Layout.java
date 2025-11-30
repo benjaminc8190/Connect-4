6f1d88b2453e186efcef6c910383ec396b5ab3c8
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-11-30 11:22:47 -0500

    set up layout

--- Layout.java
+++ Layout.java
@@ -8,13 +8,25 @@
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
 
 //Frontend of the game
 
 public class Layout extends JFrame {
-    private Game game; //Composition relationship: Layout has a Game
     public Layout(){
+        //Set up for the grid layout
+        setLayout(new GridLayout(5, 5, 5, 5));
+        setTitle("Connect4 Game");
+        JButton[][] buttons = new JButton[5][5];//2D array of buttons
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setBackground(Color.WHITE);
+                add(buttons[i][j]);//top left is (0,0) to bottom right (4,4)
+            }
+        }
+        setSize(500, 500);
+        setVisible(true);
 
+        
     }
 }
