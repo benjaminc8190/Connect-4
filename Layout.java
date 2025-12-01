79c283812b1db692147ffe7c3bfe2c8766078bde
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-12-01 14:02:50 -0400

    Call checkWin after printing the game board

--- Layout.java
+++ Layout.java
@@ -54,6 +54,7 @@ public class Layout extends JFrame {
                             }
                         }; 
                         game.printBoard();
+                        game.checkWin();
                     }
                 });
             }
